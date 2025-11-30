f05cef802e22ac17bb2e9dd57da4514d907dd244
Thomas Pfeiffer <github@pfeiffer.rocks>
2025-11-30 13:32:10 +0100

    chore: deploy stack in eu-west-1

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -7,7 +7,7 @@ const app = new cdk.App();
 new EmailInfrastructureStack(app, 'EmailInfrastructureStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: 'eu-central-1' // SES is available in eu-central-1
+    region: 'eu-west-1' // SES email receiving is only available in eu-west-1, us-east-1, us-west-2
   },
   description: 'Email infrastructure for pfeiffer.rocks domain using AWS SES'
 });
